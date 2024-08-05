95ea49048f63458b3cdf555798ceca290a72331f
Pushkar Gupta <ankurkota1712@gmail.com>
2024-08-05 19:06:26 +0530

    Add

--- wksp1.cpp
+++ wksp1.cpp
@@ -2,8 +2,20 @@
 using namespace std;
 int main()
 {
-    cout<<"hello word"<<endl;
+   /* cout<<"hello word"<<endl;
     int a=9,b=8;
     cout<<(a%b)<<endl;
-    
+    */
+   int a,b,c;
+   cout<<"Enter three numbers";
+   cin>>a>>b>>c;
+   if(a>b && a>c)
+   {
+    cout<<"A is bigger";
+
+   }
+   else 
+   cout<<"b is greater";
+
 }
+// float ka modulus nhi hotah
