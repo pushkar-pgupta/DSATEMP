12d9b51b9ee82a293b256cdd076f72a1fbf4c666
Pushkar Gupta <ankurkota1712@gmail.com>
2024-08-08 18:58:46 +0530

    day3

--- wksp3.cpp
+++ wksp3.cpp
@@ -3,16 +3,27 @@ using namespace std;
 
 int main()
 {
+    int a[3][3]= {{1,2,4,},{5,8,9},{1,5,5}};
+    int i,j;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(i==j)
+            cout<<a[i][j];
+        }
+        cout<<"\n";
+    }
 
-    int a[][5]={{1,2,3,4,5},{6,7,8,9,10}};
-    cout<<*(a)<<endl;
-    cout<<&a[0][0]<<endl;
-    cout<<**a<<endl;
-    cout<<a[0][0]<<endl;
-    cout<<(*(a)+1)<<endl;
-    cout<<&a[0][1]<<endl;
-    cout<<*(*(a)+1)<<endl;
-    cout<<a[0][1]<<endl;
+    // int a[][5]={{1,2,3,4,5},{6,7,8,9,10}};
+    // cout<<*(a)<<endl;
+    // cout<<&a[0][0]<<endl;
+    // cout<<**a<<endl;
+    // cout<<a[0][0]<<endl;
+    // cout<<(*(a)+1)<<endl;
+    // cout<<&a[0][1]<<endl;
+    // cout<<*(*(a)+1)<<endl;
+    // cout<<a[0][1]<<endl;
    
     // int a=56;
     // int *ptr,**d_ptr;
